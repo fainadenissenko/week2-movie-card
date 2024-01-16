4c27d5d2ff6f07f42429b5b5d886b6b6522800b3
Faina Denissenko <fainadns@gmai.com>
2024-01-16 21:15:54 +0200

    commit

--- week5-ModalWindow/scripts/app.js
+++ week5-ModalWindow/scripts/app.js
@@ -1,6 +1,5 @@
 //select movie poster original image
 let moviePoster = document.querySelector('.movie-poster');
-console.log(moviePoster);
 //select modal window layer
 let modal = document.querySelector('#myModal');
 //select close button
