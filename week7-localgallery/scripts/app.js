be66cf07c0b9af36b1ba47b408b55d2e7c153836
Faina Denissenko <fainadns@gmai.com>
2024-01-22 22:36:19 +0200

    commit

--- week7-localgallery/scripts/app.js
+++ week7-localgallery/scripts/app.js
@@ -7,7 +7,10 @@ const movieTitleToDisplay = document.querySelector('.favoriteMovieTitle');
 
 btn.addEventListener('click', () => {
     let movieTitleInput = movieTitle.value;
-    let posterUrlInput = moviePosterUrl.value;
-    console.log(movieTitleInput);
-    console.log(posterUrlInput);
+    let posterUrlInput = moviePosterUrl.valuealue;
+    localStorage.setItem('title', movieTitleInput);
+    localStorage.setItem('imageUrl', posterUrlInput);
+    movieTitleToDisplay.textContent = movieTitleInput;
+    container.computedStyleMap.backgroundImage = `linear-gradient(rgba(8, 8, 133, 0.486), rgba(72, 58, 58, 0.482)),
+    url('${posterUrlInput}')`;
 })
