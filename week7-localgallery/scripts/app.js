677dc296c95f457aede0fd2d438ecfcdc71a668b
Faina Denissenko <fainadns@gmai.com>
2024-01-23 20:47:10 +0200

    commit

--- week7-localgallery/scripts/app.js
+++ week7-localgallery/scripts/app.js
@@ -1,16 +1,37 @@
 const container = document.querySelector('.container');
 const movieTitle = document.querySelector('.userInputTitle');
-const moviePosterUrl = document.querySelector('.userInputPosteruser');
+const movieReleaseDate = document.querySelector('.userInputReleaseDate');
+const moviePosterUrl = document.querySelector('.userInputPosterUrl');
 const btn = document.querySelector('.button');
 const movieTitleToDisplay = document.querySelector('.favoriteMovieTitle');
+const movieReleaseDateToDisplay = document.querySelector('.favoriteMovieReleaseDate');
 
+let titleInStorage = localStorage.getItem('title');
+let releaseDateInStorage = localStorage.getItem('releaseDate');
+let imageUrlInStorage = localStorage.getItem('imageUrl');
+
+if(titleInStorage && imageUrlInStorage) {
+    movieTitleToDisplay.textContent = titleInStorage;
+    movieReleaseDateToDisplay.textContent = releaseDateInStorage;
+    container.style.backgroundImage = `linear-gradient(rgba(40, 40, 65, 0.514), rgba(63, 45, 45, 0.459)),
+    url('${imageUrlInStorage}')`;
+}
 
 btn.addEventListener('click', () => {
     let movieTitleInput = movieTitle.value;
-    let posterUrlInput = moviePosterUrl.valuealue;
+    let movieReleaseDateInput = movieReleaseDate.value;
+    let posterUrlInput = moviePosterUrl.value;
+
     localStorage.setItem('title', movieTitleInput);
-    localStorage.setItem('imageUrl', posterUrlInput);
+    localStorage.setItem('releaseDate', movieReleaseDateInput);
+    localStorage.setItem('imageUrl', posterUrlInput); 
+
     movieTitleToDisplay.textContent = movieTitleInput;
-    container.computedStyleMap.backgroundImage = `linear-gradient(rgba(8, 8, 133, 0.486), rgba(72, 58, 58, 0.482)),
+    movieReleaseDateToDisplay.textContent = movieReleaseDateInput;
+    container.style.backgroundImage = `linear-gradient(rgba(40, 40, 65, 0.514), rgba(63, 45, 45, 0.459)),
     url('${posterUrlInput}')`;
-})
+
+    movieTitle.value = '';
+    movieReleaseDate.value = '';
+    moviePosterUrl.value = '';
+});
